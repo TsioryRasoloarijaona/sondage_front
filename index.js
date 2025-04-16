ed9020c5282f8148b8e97a1a86d6f1f2b836ff4b
TsioryRasoloarijaona <hei.tsiory@gmail.com>
2025-04-16 13:45:30 +0100

    feat: adding router initialisation

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+const routes = require('./routes/routes') ;
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-}
-);
+app.use('/api' , routes) ;
 
 app.listen(port, () => {  
     console.log(`Server is running at http://localhost:${port}`);
